0978e166bb7fa260783101447e7dbf55ae22ad1a
Eugene Sia <eugene@eugenesia.co.uk>
2017-07-10 05:00:53 +0100

    Add C function and caller in loader

--- chap_03/loader.s
+++ chap_03/loader.s
@@ -30,6 +30,14 @@ loader:                         ; the loader label (defined as entry point in
     mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
     mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the
                                               ; stack (end of memory area)
+
+    external sum_of_three       ; the function sum_of_three is defined
+                                ; elsewhere
+    push dword 3                ; arg3
+    push dword 2                ; arg2
+    push dword 1                ; arg1
+    call sum_of_three           ; call the function, the result will be in eax
+
 .loop:
     jmp .loop                   ; loop forever
 
