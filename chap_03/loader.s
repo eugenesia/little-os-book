1042d05778d9c7e15914875aa656528e55a87832
Eugene Sia <eugene@eugenesia.co.uk>
2017-07-15 12:46:56 +0100

    Fix 'external' => 'extern' in nasm

--- chap_03/loader.s
+++ chap_03/loader.s
@@ -31,7 +31,7 @@ loader:                         ; the loader label (defined as entry point in
     mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the
                                               ; stack (end of memory area)
 
-    external sum_of_three       ; the function sum_of_three is defined
+    extern sum_of_three         ; the function sum_of_three is defined
                                 ; elsewhere
     push dword 3                ; arg3
     push dword 2                ; arg2
